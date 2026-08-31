75f15aa8f61353ae92dcf496ad38b939367e524c
dahamdevtools <dahamdevtools@gmail.com>
2026-08-31 19:35:23 +0530

    feat: add Categories and Income pages

---
+++ app/(dashboard)/income/page.tsx
@@ -0,0 +1,13 @@
+import { LuPlus } from "react-icons/lu";
+
+export default function Income() {
+  return (
+    <div className="w-full flex flex-col gap-7 p-3.5 pt-7">
+      <h1 className="text-xl">Income</h1>
+      <button className="w-fit h-12 px-3 gap-2 pe-5 flex items-center rounded-xl text-indigo-500 bg-indigo-100">
+        <LuPlus className="text-lg" />
+        <span>Add Income</span>
+      </button>
+    </div>
+  );
+}

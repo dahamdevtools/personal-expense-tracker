8ff05ec23f721380a791cd8740babe05a7d8b0f8
dahamdevtools <dahamdevtools@gmail.com>
2026-09-02 19:41:36 +0530

    feat: add Button, Calendar, DatePicker, and Popover components from shadcn/ui

---
+++ components/ui/datePicker.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { LuCalendar } from "react-icons/lu";
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger
+        render={
+          <Button
+            variant={"outline"}
+            data-empty={!date}
+            className="w-full font-normal hover:bg-neutral-200/50 text-base h-10 border-0 outline-0 bg-neutral-200/50 p-0 ps-4 pe-3 justify-between data-[empty=true]:text-neutral-500"
+          >
+            {date ? (
+              format(date, "PPP")
+            ) : (
+              <span className="truncate text-base font-normal">
+                Pick a date
+              </span>
+            )}
+            <LuCalendar className="text-neutral-400 size-4.5" />
+          </Button>
+        }
+      />
+      <PopoverContent
+        className="w-auto p-0 ring-0 shadow-xl shadow-neutral-900/5 bg-neutral-50"
+        align="start"
+      >
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          defaultMonth={date}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}

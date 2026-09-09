7915a40260dff443995588c5699aab0f3bf90827
dahamdevtools <dahamdevtools@gmail.com>
2026-09-09 12:48:22 +0530

    feat: enhance dashboard with income and expense fetching, loading states, and updated total calculations

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { Expense, Income } from "@/types";
+import { format } from "date-fns";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import {
   LuArrowRight,
   LuBanknote,
@@ -7,6 +12,61 @@ import {
 } from "react-icons/lu";
 
 export default function Dashboard() {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [income, setIncome] = useState<Income[]>([]);
+  const [expenseLoading, setExpenseLoading] = useState(false);
+  const [incomeLoading, setIncomeLoading] = useState(false);
+
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpense, setTotalExpenses] = useState(0);
+
+  const fetchExpenses = async () => {
+    setExpenseLoading(true);
+
+    try {
+      const res = await fetch("/api/expenses");
+      const data = await res.json();
+      setExpenses(data);
+
+      let sum = 0;
+      for (const exp of data) {
+        sum += Math.floor(exp.amount * 100);
+      }
+
+      setTotalExpenses(sum / 100);
+    } catch (error) {
+      console.error("Failed to fetch expenses", error);
+    } finally {
+      setExpenseLoading(false);
+    }
+  };
+
+  const fetchIncome = async () => {
+    setIncomeLoading(true);
+
+    try {
+      const res = await fetch("/api/income");
+      const data = await res.json();
+      setIncome(data);
+
+      let sum = 0;
+      for (const inc of data) {
+        sum += Math.floor(inc.amount * 100);
+      }
+
+      setTotalIncome(sum / 100);
+    } catch (error) {
+      console.error("Failed to fetch income", error);
+    } finally {
+      setIncomeLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+    fetchIncome();
+  }, []);
+
   return (
     <div className="w-full flex flex-col gap-7 p-3.5 pt-7 overflow-y-auto">
       <h1 className="text-xl">Dashboard</h1>
@@ -19,9 +79,12 @@ export default function Dashboard() {
             <p className="text-neutral-500 text-ellipsis line-clamp-1">
               Total Balance
             </p>
-            <p className="text-3xl font-semibold text-ellipsis line-clamp-1">
-              $91,000
-            </p>
+            <div className="font-semibold flex flex-wrap gap-2 items-end">
+              <span className="text-neutral-400">LKR</span>
+              <span className="text-3xl text-ellipsis line-clamp-1">
+                {(totalIncome - totalExpense).toFixed(2)}
+              </span>
+            </div>
           </div>
         </div>
         <div className="w-full h-fit flex items-center p-6 gap-6 rounded-2xl bg-neutral-50">
@@ -32,9 +95,12 @@ export default function Dashboard() {
             <p className="text-neutral-500 text-ellipsis line-clamp-1">
               Total Income
             </p>
-            <p className="text-3xl font-semibold text-ellipsis line-clamp-1">
-              $98,200
-            </p>
+            <div className="font-semibold flex flex-wrap gap-2 items-end">
+              <span className="text-neutral-400">LKR</span>
+              <span className="text-3xl text-ellipsis line-clamp-1">
+                {totalIncome.toFixed(2)}
+              </span>
+            </div>
           </div>
         </div>
         <div className="w-full h-fit flex items-center p-6 gap-6 rounded-2xl bg-neutral-50">
@@ -45,59 +111,16 @@ export default function Dashboard() {
             <p className="text-neutral-500 text-ellipsis line-clamp-1">
               Total Expenses
             </p>
-            <p className="text-3xl font-semibold text-ellipsis line-clamp-1">
-              $7,100
-            </p>
+            <div className="font-semibold flex flex-wrap gap-2 items-end">
+              <span className="text-neutral-400">LKR</span>
+              <span className="text-3xl text-ellipsis line-clamp-1">
+                {totalExpense.toFixed(2)}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-      <h3 className="text-lg font-semibold">Recent Transactions</h3>
-      <table className="bg-neutral-50 rounded-2xl overflow-hidden">
-        <thead>
-          <tr className="bg-neutral-200">
-            <th className="font-normal text-start p-3 px-5">Date</th>
-            <th className="font-normal text-start p-3 px-5">Category</th>
-            <th className="font-normal text-start p-3 px-5">Description</th>
-            <th className="font-normal text-start p-3 px-5">Amount</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-neutral-200">
-          <tr>
-            <td className="p-3 px-5">2026.08.31</td>
-            <td className="p-3 px-5">Shopping</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aspernatur, ab.
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex gap-1 flex-nowrap px-4 py-1 rounded-lg bg-red-100 text-red-400">
-                <span>-</span>
-                <span>$40</span>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 px-5">2026.09.01</td>
-            <td className="p-3 px-5">Freelancing</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi delectus in soluta ullam repellat totam molestias
-                sapiente maiores facilis quasi voluptas id ex corporis deleniti
-                dolor provident eveniet, omnis quos?
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex px-4 py-1 rounded-lg bg-green-100 text-green-400">
-                <span>+</span>
-                <span>$600</span>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+
       <div className="w-full h-fit flex items-center justify-between gap-6">
         <h3 className="text-lg font-semibold">Recent Income</h3>
         <Link
@@ -108,52 +131,48 @@ export default function Dashboard() {
           <LuArrowRight className="text-lg" />
         </Link>
       </div>
-      <table className="bg-neutral-50 rounded-2xl overflow-hidden">
-        <thead>
-          <tr className="bg-neutral-200">
-            <th className="font-normal text-start p-3 px-5">Date</th>
-            <th className="font-normal text-start p-3 px-5">Category</th>
-            <th className="font-normal text-start p-3 px-5">Description</th>
-            <th className="font-normal text-start p-3 px-5">Amount</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-neutral-200">
-          <tr>
-            <td className="p-3 px-5">2026.08.31</td>
-            <td className="p-3 px-5">Shopping</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aspernatur, ab.
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex gap-1 flex-nowrap px-4 py-1 rounded-lg bg-red-100 text-red-400">
-                <span>-</span>
-                <span>$40</span>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 px-5">2026.09.01</td>
-            <td className="p-3 px-5">Freelancing</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi delectus in soluta ullam repellat totam molestias
-                sapiente maiores facilis quasi voluptas id ex corporis deleniti
-                dolor provident eveniet, omnis quos?
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex px-4 py-1 rounded-lg bg-green-100 text-green-400">
-                <span>+</span>
-                <span>$600</span>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {incomeLoading ? (
+        <div className="w-full h-full p-7 text-lg flex items-center justify-center">
+          <p>Loading...</p>
+        </div>
+      ) : income.length === 0 ? (
+        <div className="w-full h-full p-7 text-lg flex items-center justify-center">
+          <p>No income yet.</p>
+        </div>
+      ) : (
+        <table className="bg-neutral-50 rounded-2xl overflow-hidden">
+          <thead>
+            <tr className="bg-neutral-200">
+              <th className="font-normal text-start p-3 px-5">Date</th>
+              <th className="font-normal text-start p-3 px-5">Category</th>
+              <th className="font-normal text-start p-3 px-5">Description</th>
+              <th className="font-normal text-start p-3 px-5">Amount</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-neutral-200">
+            {income.map((inc) => (
+              <tr key={inc.id}>
+                <td className="p-3 px-5">
+                  {format(new Date(inc.date), "MMM dd, yyyy")}
+                </td>
+                <td className="p-3 px-5">{inc.category}</td>
+                <td className="p-3 px-5">
+                  <p className="text-ellipsis line-clamp-1">
+                    {inc.description}
+                  </p>
+                </td>
+                <td className="p-2 ps-5">
+                  <span className="w-fit h-fit flex gap-1 flex-nowrap px-4 py-1 rounded-lg bg-green-100 text-green-400">
+                    <span>+</span>
+                    <span>LKR {inc.amount}</span>
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
       <div className="w-full h-fit flex items-center justify-between gap-6">
         <h3 className="text-lg font-semibold">Recent Expenses</h3>
         <Link
@@ -164,52 +183,47 @@ export default function Dashboard() {
           <LuArrowRight className="text-lg" />
         </Link>
       </div>
-      <table className="bg-neutral-50 rounded-2xl overflow-hidden">
-        <thead>
-          <tr className="bg-neutral-200">
-            <th className="font-normal text-start p-3 px-5">Date</th>
-            <th className="font-normal text-start p-3 px-5">Category</th>
-            <th className="font-normal text-start p-3 px-5">Description</th>
-            <th className="font-normal text-start p-3 px-5">Amount</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-neutral-200">
-          <tr>
-            <td className="p-3 px-5">2026.08.31</td>
-            <td className="p-3 px-5">Shopping</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aspernatur, ab.
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex gap-1 flex-nowrap px-4 py-1 rounded-lg bg-red-100 text-red-400">
-                <span>-</span>
-                <span>$40</span>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 px-5">2026.09.01</td>
-            <td className="p-3 px-5">Freelancing</td>
-            <td className="p-3 px-5">
-              <p className="text-ellipsis line-clamp-1">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi delectus in soluta ullam repellat totam molestias
-                sapiente maiores facilis quasi voluptas id ex corporis deleniti
-                dolor provident eveniet, omnis quos?
-              </p>
-            </td>
-            <td className="p-2 ps-5">
-              <span className="w-fit h-fit flex px-4 py-1 rounded-lg bg-green-100 text-green-400">
-                <span>+</span>
-                <span>$600</span>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {expenseLoading ? (
+        <div className="w-full h-full p-7 text-lg flex items-center justify-center">
+          <p>Loading...</p>
+        </div>
+      ) : expenses.length === 0 ? (
+        <div className="w-full h-full p-7 text-lg flex items-center justify-center">
+          <p>No expense yet.</p>
+        </div>
+      ) : (
+        <table className="bg-neutral-50 rounded-2xl overflow-hidden">
+          <thead>
+            <tr className="bg-neutral-200">
+              <th className="font-normal text-start p-3 px-5">Date</th>
+              <th className="font-normal text-start p-3 px-5">Category</th>
+              <th className="font-normal text-start p-3 px-5">Description</th>
+              <th className="font-normal text-start p-3 px-5">Amount</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-neutral-200">
+            {expenses.map((expense) => (
+              <tr key={expense.id}>
+                <td className="p-3 px-5">
+                  {format(new Date(expense.date), "MMM dd, yyyy")}
+                </td>
+                <td className="p-3 px-5">{expense.category}</td>
+                <td className="p-3 px-5">
+                  <p className="text-ellipsis line-clamp-1">
+                    {expense.description}
+                  </p>
+                </td>
+                <td className="p-2 ps-5">
+                  <span className="w-fit h-fit flex gap-1 flex-nowrap px-4 py-1 rounded-lg bg-red-100 text-red-400">
+                    <span>-</span>
+                    <span>LKR {expense.amount}</span>
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }

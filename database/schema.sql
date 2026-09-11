5a9f859f099baa6dbd9d79b7fc104c4dfe4e153e
dahamdevtools <dahamdevtools@gmail.com>
2026-09-11 19:06:42 +0530

    feat: increase password field length to support stronger passwords

--- database/schema.sql
+++ database/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `expense_tracker`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(100) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `currency` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;

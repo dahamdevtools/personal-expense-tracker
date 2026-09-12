83b45edcd50edfea8bf51b4e24eea0638d16ac86
dahamdevtools <dahamdevtools@gmail.com>
2026-09-12 13:44:57 +0530

    feat: update dashboard layout to display current user's username

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,16 @@
 import SideMenu from "@/components/sideMenu";
+import { getCurrentUser } from "@/lib/auth";
 import { LuUser } from "react-icons/lu";
 
-export default function DashboardLayout({ children }: LayoutProps<"/">) {
+export default async function DashboardLayout({ children }: LayoutProps<"/">) {
+  const session = await getCurrentUser();
+
   return (
     <div className="w-full max-w-360 h-screen overflow-hidden flex px-3.5 gap-3.5">
       <SideMenu />
       <main className="flex-1 flex flex-col pt-3.5">
         <div className="w-full h-fit flex items-center justify-between gap-4 ps-6 p-3.5 rounded-2xl bg-neutral-50">
-          <p className="text-neutral-500">Welcome User!</p>
+          <p className="text-neutral-500">Welcome {session?.username}!</p>
           <button className="w-10 h-10 aspect-square rounded-full flex items-center justify-center text-xl border-4 border-indigo-200 bg-indigo-400 text-indigo-50">
             <LuUser />
           </button>

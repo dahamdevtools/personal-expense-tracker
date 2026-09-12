96d2ef01a91d4f6407f3c19b5433a71b06c4fff0
dahamdevtools <dahamdevtools@gmail.com>
2026-09-12 12:18:04 +0530

    feat: enhance signup page with form handling and error display

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,8 +1,50 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSignUp = async () => {
+    setError("");
+    setLoading(true);
+
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          username: name,
+          email,
+          password,
+          currency,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.error);
+        return;
+      }
+
+      router.push("/login");
+    } catch (error) {
+      console.error("Failed to create account", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-full h-full flex-1 flex items-center justify-center p-7 overflow-y-scroll">
       <div className="w-full max-w-sm h-fit flex flex-col items-center p-7 gap-7 rounded-2xl bg-neutral-50">
@@ -23,6 +65,8 @@ export default function SignUp() {
               spellCheck="false"
               className="w-full h-10 rounded-xl px-4 truncate bg-neutral-200/50"
               placeholder="Enter your Name..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="w-full h-fit flex flex-col gap-2">
@@ -33,6 +77,8 @@ export default function SignUp() {
               spellCheck="false"
               className="w-full h-10 rounded-xl px-4 truncate bg-neutral-200/50"
               placeholder="Enter your Email..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="w-full h-fit flex flex-col gap-2">
@@ -43,6 +89,8 @@ export default function SignUp() {
               spellCheck="false"
               className="w-full h-10 rounded-xl px-4 truncate bg-neutral-200/50"
               placeholder="Enter your Password..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="w-full h-fit flex flex-col gap-2">
@@ -53,15 +101,22 @@ export default function SignUp() {
               spellCheck="false"
               className="w-full h-10 rounded-xl px-4 truncate bg-neutral-200/50"
               placeholder="USD, AUD, LKR, $..."
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
             />
           </div>
         </div>
+        {error && <p className="text-red-400">{error}</p>}
         <Link href={"/login"} className="text-center">
           <span className="text-neutral-500">Already have an Account?</span>{" "}
           <span className="underline">Login here</span>
         </Link>
-        <button className="w-full h-10 disabled:opacity-50 rounded-xl px-5 text-indigo-500 bg-indigo-100">
-          Create Account
+        <button
+          onClick={handleSignUp}
+          disabled={loading}
+          className="w-full h-10 disabled:opacity-50 rounded-xl px-5 text-indigo-500 bg-indigo-100"
+        >
+          {loading ? "Creating Account..." : "Create Account"}
         </button>
       </div>
     </div>
